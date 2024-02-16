cc28bf939dffaecd3f4d813c6deeb48aacb21d1b
Ritwik <ritwikhero@gmail.com>
2024-02-16 14:15:47 +0530

    datatypes notes added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all code as new version of js , its evolved so no need to use but a good use case of it 
+
+//alert(3+3);  we are using node js not browser 
+
+//code readability should be given high priority
+
+let name = "Ritwik"; //string
+let age = 26; //integer
+let isLoggeIn = true; //boolean
+
+//null => is stand alone value
+//undefined => variable is created but not value is given
+
+console.log(typeof name);
+console.log(typeof age);
