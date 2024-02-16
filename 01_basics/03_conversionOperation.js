c9b2bf82d4e460f8fa04b5a03c1ad18889a4935b
Ritwik <ritwikhero@gmail.com>
2024-02-16 17:26:37 +0530

    basics of conversion of datatypes added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+//NUMBER CONVERSION ---------------------------------------------------
+
+// let score = 69;
+//let score = "69abc" 
+// let score = false;
+// let score = "hello";
+// // console.log(typeof(score));
+
+// let scoreNumber = Number(score); // conversion of string in number data type
+// // console.log(typeof(scoreNumber));
+// console.log(scoreNumber);
+
+// "69" => 69
+//"69abc" => NaN
+// true => 1 & false => 0
+
+//BOOLEAN CONVERSION----------------------------------------------------
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(isLoggedIn));
+// console.log(typeof(booleanIsLoggedIn));
+
+// 1 => true; 0 => false
+// "" => false; "ahahha" => true
+
+//STRING CONVERSION ----------------------------------------------------
+
+let num = 69;
+let stringNum = String(num);
+console.log(typeof(num));
+console.log(typeof(stringNum));
+
+
+
