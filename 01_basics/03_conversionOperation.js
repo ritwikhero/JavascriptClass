243fbad51ddbf7e2130a11872d8687a84647dd4d
Ritwik <ritwikhero@gmail.com>
2024-02-20 17:22:49 +0530

    some more conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,30 +1,30 @@
 //NUMBER CONVERSION ---------------------------------------------------
 
 // let score = 69;
-//let score = "69abc" 
+// let score = "69abc" 
 // let score = false;
-// let score = "hello";
-// // console.log(typeof(score));
+let score = "hello";
+// console.log(typeof(score));
 
-// let scoreNumber = Number(score); // conversion of string in number data type
-// // console.log(typeof(scoreNumber));
-// console.log(scoreNumber);
+let scoreNumber = Number(score); // conversion of string in number data type
+// console.log(typeof(scoreNumber));
+console.log(scoreNumber);
 
 // "69" => 69
 //"69abc" => NaN
 // true => 1 & false => 0
 
-//BOOLEAN CONVERSION----------------------------------------------------
+// //BOOLEAN CONVERSION----------------------------------------------------
 
-// let isLoggedIn = 1;
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(typeof(isLoggedIn));
-// console.log(typeof(booleanIsLoggedIn));
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(isLoggedIn));
+console.log(typeof(booleanIsLoggedIn));
 
 // 1 => true; 0 => false
 // "" => false; "ahahha" => true
 
-//STRING CONVERSION ----------------------------------------------------
+// //STRING CONVERSION ----------------------------------------------------
 
 let num = 69;
 let stringNum = String(num);
@@ -32,4 +32,22 @@ console.log(typeof(num));
 console.log(typeof(stringNum));
 
 
+//OPERATION -------------------------------------------------------------
+let value = 1;
+let negVlaue = -value;
+console.log(negVlaue);
 
+let s1 = "ritwik";
+let s2 = " singh";
+console.log(s1+s2);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let counter = 100;
+counter++;
+console.log(counter);
