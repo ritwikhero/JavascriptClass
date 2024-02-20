9134236f05fd2f84655fe60f11aab25c13cd25e4
Ritwik <ritwikhero@gmail.com>
2024-02-21 01:15:41 +0530

    comparison operation notes added

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+//basic comparison --------------------------------
+console.log(2 == 2);   
+console.log(2 >= 5);
+console.log(2 <= 4);
+console.log(2 != 2);
+console.log(2 < 3);
+console.log(2 > 3);
+
+
+//something worth noting , here comparison treats null as 0 while equality treats it differently
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+//same thing doesn't happen in undefined
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined != 0);
+
+// === is called strict checks datatype
+
+console.log("2" === "2");
+
+
+
+
+
+
