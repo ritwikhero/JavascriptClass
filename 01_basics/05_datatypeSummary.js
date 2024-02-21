14406e7b3198f19b99edab133eea666b7cfc903b
Ritwik <ritwikhero@gmail.com>
2024-02-21 17:12:28 +0530

    Memory in JS added

--- 01_basics/05_datatypeSummary.js
+++ 01_basics/05_datatypeSummary.js
@@ -19,10 +19,10 @@ console.log(typeof(isLoggedIn));
 console.log(typeof(outsideTemp));
 console.log(typeof(userName)); //undefined cause no value assingned
 
-let bigNum = 34343434343434343434343434344343434343434n
+// let bigNum = 34343434343434343434343434344343434343434n
 //Reference (Non Primitive)--------------------------------------------------------
 
-//Arrays, Objects, Functions
+Arrays, Objects, Functions
 
 const heroes = ["spiderman","batman","superman"];
 
@@ -43,3 +43,29 @@ console.log(typeof(heroes));
 console.log(typeof(myObject));
 console.log(typeof(myFunction));
 console.log(typeof(bigNum));
+
+
+
+// Memory in JavaScript ---------------------------------------------------------
+//Stack (Primitive) && Heap(Non-Primitive)
+
+let userName = "user@email.com"
+
+let email = userName;
+
+email = "noreply@email.com"
+
+console.log(email);
+console.log(userName);
+
+let userOne = {
+    email : "userOne@google.com",
+    upi  : "userOne@oksbi"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "userTwo@yahoo.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
