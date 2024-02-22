159fb7a8e8b8015d35db6aa43d6444f966518805
Ritwik <ritwikhero@gmail.com>
2024-02-23 01:14:02 +0530

    Some string functions explored

--- 01_basics/06_Strings.js
+++ 01_basics/06_Strings.js
@@ -5,6 +5,26 @@ const repoCount = 26;
 //old way ------
 // console.log("My name is "+name+" and my repoCout is "+repoCount);
 
-//new / better way
+// new / better way / string interpolation
 
 console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String("Ritwik");
+console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("w"));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(1,4);
+console.log(anotherString);
+
+const s = "              ritwik               ";
+console.log(s);
+console.log(s.trim());
