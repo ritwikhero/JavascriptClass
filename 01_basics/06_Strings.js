a187571e294cdb483002acc1c4348afb4d52d7aa
Ritwik <ritwikhero@gmail.com>
2024-02-26 01:11:34 +0530

    some number methods visited

--- 01_basics/06_Strings.js
+++ 01_basics/06_Strings.js
@@ -9,7 +9,7 @@ const repoCount = 26;
 
 console.log(`My name is ${name} and my repoCount is ${repoCount}`);
 
-const gameName = new String("Ritwik");
+const gameName = new String("Ritwik-Ranjan-Singh");
 console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
@@ -32,3 +32,4 @@ console.log(s.trim());
 const url = "https:/www.ritwik.com%20/abcd"
 console.log(url.replace("%","-"));
 console.log(url.includes("ritwik"));
+console.log(gameName.split("-"));
---
+++ 01_basics/07_Number_Maths.js
@@ -0,0 +1,12 @@
+const score = 500;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNum = 12123.349;
+console.log(otherNum.toPrecision(4));
+
