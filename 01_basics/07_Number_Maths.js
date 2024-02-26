1d9cd516d71ea39fb5970a0cf0fcfb4b68c6c3f5
Ritwik <ritwikhero@gmail.com>
2024-02-26 14:17:38 +0530

    Number and maths functions visited

--- 01_basics/07_Number_Maths.js
+++ 01_basics/07_Number_Maths.js
@@ -1,3 +1,4 @@
+//Number--------------------------------------------------------------------
 const score = 500;
 console.log(score);
 
@@ -9,4 +10,31 @@ console.log(balance.toFixed(2));
 
 const otherNum = 12123.349;
 console.log(otherNum.toPrecision(4));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString("en-IN"));
+
+console.log(Number.MAX_VALUE); // many other function with number are there
+
+//Maths-----------------------------------------------------------------------
+
+console.log(Math);
+console.log(Math.abs(-1));
+console.log(Math.round(6.5));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(6.9));
+console.log(Math.min(2,6,8,4,1));
+console.log(Math.max(2,6,8,4,1));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
 
