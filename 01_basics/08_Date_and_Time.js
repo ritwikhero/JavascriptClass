0ad173b6d412f210365119005de135d519dc7390
Ritwik <ritwikhero@gmail.com>
2024-02-26 17:33:49 +0530

    Started with date class

---
+++ 01_basics/08_Date_and_Time.js
@@ -0,0 +1,31 @@
+//Dates
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate));
+
+let myCreatedDate = new Date(2024,1,26,17,1);
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+// console.log(newDate.getDate());
+
+// console.log(`date is ${newDate.getDate()} and time is ${newDate.getTime()}`);
+
+
+
