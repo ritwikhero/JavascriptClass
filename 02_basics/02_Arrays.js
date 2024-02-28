9e75a415fc6abfa8d5eb263f822665f61d921e7f
Ritwik <ritwikhero@gmail.com>
2024-02-28 23:28:14 +0530

    exploring array

--- 02_basics/02_Arrays.js
+++ 02_basics/02_Arrays.js
@@ -1,9 +1,9 @@
 const marvelHeroes = ["spiderman","ironman","thor"];
 const dcHeroes = ["batman","superman","flash"];
 
-// marvelHeroes.push(dcHeroes);
+marvelHeroes.push(dcHeroes);
 
-// console.log(marvelHeroes);
+console.log(marvelHeroes);
 
 const assemble = marvelHeroes.concat(dcHeroes); //concat 2 arrays
 console.log(assemble);
@@ -15,3 +15,13 @@ console.log(newArr);
 const arr = [1,2,3,[4,5,6],7,[8,9,10,[11,12,13]]];
 const arr1 = arr.flat(Infinity);
 console.log(arr1);
+
+console.log(Array.isArray("Ritwik"));
+console.log(Array.from("Ritwik"));
+console.log(Array.from({name : "Ritwik"})); // interesting , will return empty object cause it doesn't know what to convert the key or the value to array
+
+let s1 = 100;
+let s2 = 99;
+let s3 = 98;
+
+console.log(Array.of(s1,s2,s3));
