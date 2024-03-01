bad20a93ca34e50756576824c129f094991f15d5
Ritwik <ritwikhero@gmail.com>
2024-03-01 23:50:18 +0530

    objects class 1 done

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -5,9 +5,31 @@
 // Object.create()  --> another wayof creating objects
 
 //they are defined in key value pairs
+
+const mySym = Symbol("Key1");
 const User = {
     name : "Ritwik",
+    "full name" : "Ritwik Singh",
+    [mySym] : "myKey1", //syntax for writing symbol
     age : 26,
     email : "ritwik@google.com",
-    isLoggedIn : false;
+    isLoggedIn : false,
 }
+
+console.log(User.email); //should be avoided 
+console.log(User["email"]); // is generally preffered, " " cause js treats them as string
+console.log(User["full name"]) // cause of this problem 
+// console.log(typeof User[mySym]);
+console.log(User[mySym]);
+
+
+User.email = "ritwik@email.com";
+// Object.freeze(User); //after this no value will be changed in the object
+User.email = "ritwik@femail.com";
+console.log(User);
+
+User.greeting = function(){
+    console.log(`hello user, ${this.name}`);
+}
+
+console.log(User.greeting());
