2c5ec8d914844f45c65f5250e58f451e0355e159
Ritwik <ritwikhero@gmail.com>
2024-03-01 22:29:11 +0530

    obect created

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -1,1 +1,13 @@
 //sigleton
+
+//object literal
+
+// Object.create()  --> another wayof creating objects
+
+//they are defined in key value pairs
+const User = {
+    name : "Ritwik",
+    age : 26,
+    email : "ritwik@google.com",
+    isLoggedIn : false;
+}
