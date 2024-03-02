c19639c61bbf8328e74e40cda967a6632bd2354c
Ritwik <ritwikhero@gmail.com>
2024-03-02 23:28:18 +0530

    objects continued

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -20,3 +20,28 @@ const user = {
 
 // console.log(user.name.fullname.lastName);
 
+const obj1 = {1:"a",2:"b"};
+const obj2 = {3:"c",4:"d"};
+
+// const obj3 = {obj1,obj2}; //creates nestes objects
+// const obj3  = Object.assign({},obj1,obj2); //assings object to a ew empty object
+
+//spread operator
+const obj3 = {...obj1,...obj2};
+// console.log(obj3);
+
+const users = [
+    {},
+    {
+        id : "1",
+        email: "r@email.com"
+    },
+    {},{},
+]
+// console.log(users[1]);
+
+console.log(tUser);
+console.log(Object.keys(tUser));
+console.log(Object.values(tUser));
+console.log(Object.entries(tUser));
+console.log(Object.hasOwnProperty("isLoggedIn"));
