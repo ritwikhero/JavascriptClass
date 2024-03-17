babdc04b105392f80e498cc73360da227d9c6d52
Ritwik <ritwikhero@gmail.com>
2024-03-17 23:14:57 +0530

    done with function

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -9,3 +9,32 @@ function sum(a,b){
 }
 
 console.log(sum(1,1));
+
+//rest operator
+function calcualteCartPrice(...num1){
+    return num1;
+}
+//returns an array of the given input
+console.log(calcualteCartPrice(100,200,400,500,2000));
+
+const user = {
+    username : "Ritwik",
+    age : "26"
+}
+
+
+function handleObject(object){
+    console.log(`the name is ${object.username} and the age is ${object.age}`);
+}
+//type check is important here the attributes should be present in the object even the spelling should be macthed
+handleObject(user);
+
+
+const arr = [100,200,400,500,2000];
+
+function returnSecond(num){
+    return num[1];
+}
+
+console.log(returnSecond(arr));
+
