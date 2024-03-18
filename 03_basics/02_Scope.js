ba21e50174a7b8c5b1c3279e192dd16a33e7e2f9
Ritwik <ritwikhero@gmail.com>
2024-03-18 23:22:16 +0530

    started with scope

---
+++ 03_basics/02_Scope.js
@@ -0,0 +1,20 @@
+let a = 10;
+const b = 20;
+var c = 30;
+
+console.log(a);
+console.log(b);
+console.log(c);
+// {} ==> scope of a lnaguage
+
+if(true){
+    // let a = 10;
+    const b = 20;
+    var c = 30; 
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+//var is avoid as it violates scope
+
