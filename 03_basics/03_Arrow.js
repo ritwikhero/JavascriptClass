8642da28bc452570a551cef20ac7a65cdcc808c9
Ritwik <ritwikhero@gmail.com>
2024-05-12 23:22:05 +0530

    Arrow fn continued

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -2,4 +2,13 @@ const user = {
     username : "Ritwik",
     price : 999,
     
+    welcomeMessage : function(){
+        console.log(`${this.username} welcome to website`); //this refers current context here
+        console.log(this);
+    }
 }
+user.welcomeMessage();
+user.username = "Rishik";
+user.welcomeMessage();
+
+console.log(this);
