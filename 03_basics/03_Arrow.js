ca3e87dbbd1dd6150b4b2f9c1f95543b5908cf26
Ritwik <ritwikhero@gmail.com>
2024-05-19 23:52:03 +0530

    Arrow fn over almost

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -1,14 +1,38 @@
-const user = {
-    username : "Ritwik",
-    price : 999,
+// const user = {
+//     username : "Ritwik",
+//     price : 999,
     
-    welcomeMessage : function(){
-        console.log(`${this.username} welcome to website`); //this refers current context here
-        console.log(this);
-    }
-}
-user.welcomeMessage();
-user.username = "Rishik";
-user.welcomeMessage();
+//     welcomeMessage : function(){
+//         console.log(`${this.username} welcome to website`); //this refers current context here
+//         console.log(this);
+//     }
+// }
+// user.welcomeMessage();
+// user.username = "Rishik";
+// user.welcomeMessage();
 
-console.log(this);
+// console.log(this);
+
+// Arrow function-------------------------------------------------------->
+
+// function chai(){
+//     let username = "Ritwik"
+//     console.log(this.username);
+// }
+
+const chai = () => {
+    let username = "Ritwik"
+    console.log(this);
+}
+chai();
+
+//explict arrow fn
+const addTwo = (a,b) =>{
+    return a + b;
+}
+
+//implicit arrow fn
+
+const addTwo = (a,b) => (a+b);
+
+console.log(addTwo(1,2));
